77acfe065ebbfae3da907cf2d7b6d72304305db1
ShintaNakama <shintanakama77@gmail.com>
2019-12-22 23:29:23 +0900

    wesocket chat

--- chat/main.go
+++ chat/main.go
@@ -1,25 +1,48 @@
 package main
 
 import (
+	"flag"
+	"html/template"
 	"log"
 	"net/http"
+	"os"
+	"path/filepath"
+	"sync"
+
+	"github.com/ShintaNakama/oreilly-webapp/trace"
 )
 
-func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-      <html>
-        <head>
-          <title>chat</title>
-        </head>
-        <body>
-          year!!!
-        </body>
-      </html>
-    `))
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+// ServeHTTPはHTTPリクエストを返します
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	log.Println(t.filename)
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("/Users/nakamashinta/go/src/github.com/ShintaNakama/oreilly-webapp/chat/templates", t.filename)))
 	})
-	// webサーバーを開始する
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal("ListenAndServe;", err)
+	t.templ.Execute(w, r)
+}
+
+func main() {
+	var addr = flag.String("addr", ":8080", "The addr or the application.")
+	// parde the flags
+	flag.Parse()
+
+	r := newRoom()
+	r.tracer = trace.New(os.Stdout)
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+
+	// get the room going
+	go r.run()
+
+	// start the web server
+	log.Println("Starting web server on", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe:", err)
 	}
 }
