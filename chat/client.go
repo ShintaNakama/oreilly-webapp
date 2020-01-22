9b98aaea04627b49a0c962a43c382004701c7ee9
ShintaNakama <shintanakama77@gmail.com>
2020-01-23 01:54:04 +0900

    godotenv導入、チャットに名前入れる

--- chat/client.go
+++ chat/client.go
@@ -1,30 +1,37 @@
 package main
 
-import "github.com/gorilla/websocket"
+import (
+	"time"
+
+	"github.com/gorilla/websocket"
+)
 
 type client struct {
-	socket *websocket.Conn
-	send   chan []byte
-	room   *room
+	socket   *websocket.Conn
+	send     chan *message
+	room     *room
+	userData map[string]interface{}
 }
 
 func (c *client) read() {
 	defer c.socket.Close()
 	for {
-		_, msg, err := c.socket.ReadMessage()
-		if err != nil {
-			return
+		var msg *message
+		if err := c.socket.ReadJSON(&msg); err == nil {
+			msg.When = time.Now()
+			msg.Name = c.userData["name"].(string)
+			c.room.forward <- msg
+		} else {
+			break
 		}
-		c.room.forward <- msg
 	}
 }
 
 func (c *client) write() {
 	defer c.socket.Close()
 	for msg := range c.send {
-		err := c.socket.WriteMessage(websocket.TextMessage, msg)
-		if err != nil {
-			return
+		if err := c.socket.WriteJSON(msg); err != nil {
+			break
 		}
 	}
 
--- chat/main.go
+++ chat/main.go
@@ -10,6 +10,10 @@ import (
 	"sync"
 
 	"github.com/ShintaNakama/oreilly-webapp/trace"
+	"github.com/joho/godotenv"
+	"github.com/stretchr/gomniauth"
+	"github.com/stretchr/gomniauth/providers/google"
+	"github.com/stretchr/objx"
 )
 
 type templateHandler struct {
@@ -18,19 +22,39 @@ type templateHandler struct {
 	templ    *template.Template
 }
 
+func Env_load() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
 // ServeHTTPはHTTPリクエストを返します
 func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	log.Println(t.filename)
 	t.once.Do(func() {
 		t.templ = template.Must(template.ParseFiles(filepath.Join("/Users/nakamashinta/go/src/github.com/ShintaNakama/oreilly-webapp/chat/templates", t.filename)))
 	})
-	t.templ.Execute(w, r)
+	data := map[string]interface{}{
+		"Host": r.Host,
+	}
+	if authCookie, err := r.Cookie("auth"); err == nil {
+		data["UserData"] = objx.MustFromBase64(authCookie.Value)
+	}
+	t.templ.Execute(w, data)
 }
 
+var host = flag.String("host", ":8080", "The addr or the application.")
+
 func main() {
-	var addr = flag.String("addr", ":8080", "The addr or the application.")
+	Env_load()
 	// parde the flags
 	flag.Parse()
+	gomniauth.SetSecurityKey(os.Getenv("GOMNIAUTH_SECURITY_KEY"))
+	gomniauth.WithProviders(
+		//github.New(),
+		google.New(os.Getenv("CLIENT_ID"), os.Getenv("SEACRET_KEY"), "http://localhost:8080/auth/callback/google"),
+		//faceook.New(),
+	)
 
 	r := newRoom()
 	r.tracer = trace.New(os.Stdout)
@@ -43,8 +67,8 @@ func main() {
 	go r.run()
 
 	// start the web server
-	log.Println("Starting web server on", *addr)
-	if err := http.ListenAndServe(*addr, nil); err != nil {
+	log.Println("Starting web server on", *host)
+	if err := http.ListenAndServe(*host, nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
 	}
 }
