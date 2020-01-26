82f1089271dc7508eeb8702b324fad56c1624d17
ShintaNakama <shintanakama77@gmail.com>
2020-01-27 00:16:24 +0900

    test修正

--- chat/avatar_test.go
+++ chat/avatar_test.go
@@ -4,24 +4,27 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+
 	"testing"
+
+	gomniauthtest "github.com/stretchr/gomniauth/test"
 )
 
 func TestAuthAvatar(t *testing.T) {
 	var authAvatar AuthAvatar
 	testUser := &gomniauthtest.TestUser{}
-	testUser.On("AvatarURL").Return("", ErrNoAvatorURL)
+	testUser.On("AvatarURL").Return("", ErrNoAvatarURL)
 	testChatUser := &chatUser{User: testUser}
-	url, err := authAvatar.GetAvatarURL(testChatuser)
+	url, err := authAvatar.GetAvatarURL(testChatUser)
 	if err != ErrNoAvatarURL {
 		t.Error("AuthAvatar.GetAvatarURL should return ErrNoAvatarURL when no value present")
 	}
 
 	testURL := "http://url-to-gravatar/"
 	testUser = &gomniauthtest.TestUser{}
-	testChatuser.User = testUser
+	testChatUser.User = testUser
 	testUser.On("AvatarURL").Return(testURL, nil)
-	url, err = authAvatar.GetAvatarURL(testChatuser)
+	url, err = authAvatar.GetAvatarURL(testChatUser)
 	if err != nil {
 		t.Error("AuthAvatar.GetAvataURL should return no error when value present")
 	}
@@ -42,7 +45,7 @@ func TestGravatarAvatar(t *testing.T) {
 	}
 }
 func TestFileSystemAvatar(t *testing.T) {
-	filename := filepath.Join("avatar", "abc.jpg")
+	filename := filepath.Join("avatars", "abc.jpg")
 	if err := os.MkdirAll("avatars", 0777); err != nil {
 		t.Errorf("couldn't make avatar: %s", err)
 	}
@@ -51,7 +54,7 @@ func TestFileSystemAvatar(t *testing.T) {
 	}
 	defer os.Remove(filename)
 
-	var fileSystemAvatar FileSystemAvatarv
+	var fileSystemAvatar FileSystemAvatar
 	user := &chatUser{uniqueID: "abc"}
 	url, err := fileSystemAvatar.GetAvatarURL(user)
 	if err != nil {
